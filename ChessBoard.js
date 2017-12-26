e1efcf0a2bbe1e74fd05320ceec92f16f17dc133
Bhadresh Dudhat <33336465+bhadreshdudhat@users.noreply.github.com>
2017-12-26 15:13:45 -0600

    Create Piece object

--- ChessBoard.js
+++ ChessBoard.js
@@ -9,8 +9,8 @@ function Tile(f,r){
 };
 Tile.prototype.print=function(){
     console.log("------")
-    console.log("File "+this.file);
-    console.log("Row "+this.row);
+    console.log("File: "+this.file);
+    console.log("Row: "+this.row);
 };
 Tile.prototype.corrupted=function(){
     if(this.row>8 || this.row<1||this.file<'a'||this.file>'h'){
@@ -19,13 +19,35 @@ Tile.prototype.corrupted=function(){
     return false;
 };
 
-var t1=new Tile('b',5);
-var t2=new Tile('k',3);
 
+///////////////////////////////////////////
+//Piece Object
+function Piece(c,t,l){
+    this.color=c;
+    this.type=t;
+    this.location=l;//this.location is of type Tile
+    this.location.Piece=this;// location object's piece having same piece obj created
+    //set up is done in Piece constructor and not in Tile constructor
+};
 
-t1.print();
-t2.print();
-console.log("T1 is corrupt - "+t1.corrupted());
-console.log("T2 is corrupt - "+t2.corrupted());
+Piece.prototype.print=function(){
+    console.log("------")
+    console.log("color: "+this.color);
+    console.log("type: "+this.type);
+    this.location.print();//this.location is of type Tile
+};
+Piece.prototype.corrupted=function(){
+    if(this.type=="rook" ||this.type=="queen" ||this.type=="bishop" ||this.type=="pawn" ||this.type=="knight" ||this.type=="king"){
+        return true;
+    }
+    return false;
+};
 
-//////////////////////////////////////////
+var l1=new Tile('a',5);
+var l2=new Tile('g',3);
+
+var p1=new Piece('black','q',l1);
+var p2=new Piece('white','p',l2);
+
+p1.print();
+p2.print();
